f0ed5a9385de32543b8ab7c0bb2a564af57dc609
David Mändlen <david.maendlen@bifroe.st>
2022-12-19 10:53:16 +0100

    clean up unnecessary stringification of timezone offsets

--- dtg.py
+++ dtg.py
@@ -12,38 +12,38 @@ def get_timezone_letter(now: datetime):
     Get timezone letter for the timezone of a datetime object.
     """
 
-    timezones = dict([('-12.0', 'Y'),
-                      ('-11.0', 'X'),
-                      ('-10.0', 'W'),
-                      ('-9.0', 'V'),
-                      ('-8.0', 'U'),
-                      ('-7.0', 'T'),
-                      ('-6.0', 'S'),
-                      ('-5.0', 'R'),
-                      ('-4.0', 'Q'),
-                      ('-3.0', 'P'),
-                      ('-2.0', 'O'),
-                      ('-1.0', 'N'),
-                      ('0.0', 'Z'),
-                      ('1.0', 'A'),
-                      ('2.0', 'B'),
-                      ('3.0', 'C'),
-                      ('4.0', 'D'),
-                      ('5.0', 'E'),
-                      ('6.0', 'F'),
-                      ('7.0', 'G'),
-                      ('8.0', 'H'),
-                      ('9.0', 'I'),
-                      ('10.0', 'K'),
-                      ('11.0', 'L'),
-                      ('12.0', 'M')])
+    timezones = dict([(-12.0, 'Y'),
+                      (-11.0, 'X'),
+                      (-10.0, 'W'),
+                      (-9.0, 'V'),
+                      (-8.0, 'U'),
+                      (-7.0, 'T'),
+                      (-6.0, 'S'),
+                      (-5.0, 'R'),
+                      (-4.0, 'Q'),
+                      (-3.0, 'P'),
+                      (-2.0, 'O'),
+                      (-1.0, 'N'),
+                      (0.0, 'Z'),
+                      (1.0, 'A'),
+                      (2.0, 'B'),
+                      (3.0, 'C'),
+                      (4.0, 'D'),
+                      (5.0, 'E'),
+                      (6.0, 'F'),
+                      (7.0, 'G'),
+                      (8.0, 'H'),
+                      (9.0, 'I'),
+                      (10.0, 'K'),
+                      (11.0, 'L'),
+                      (12.0, 'M')])
 
     offset = now.astimezone().utcoffset().seconds / 3600 # 3600 seconds per hour
     if now.astimezone().utcoffset().days:
         # subtract a full day
         offset = offset - 24
 
-    return timezones[str(offset)]
+    return timezones[offset]
 
 def set_time():
     """
